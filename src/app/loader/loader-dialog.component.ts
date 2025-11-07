1bce7076fec73c2ac5203f089c530f7fd69c762c
ShreyasVH <shreyas.deve.1@gmail.com>
2025-11-07 21:34:45 +0530

    loader support

---
+++ src/app/loader/loader-dialog.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-loader-dialog',
+    templateUrl: './loader-dialog.component.html'
+})
+export class LoaderDialogComponent {}
