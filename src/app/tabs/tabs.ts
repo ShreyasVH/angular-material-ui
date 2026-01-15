3076f696877a30315a777945753a42bb3e4f0ebd
ShreyasVH <shreyas.deve.1@gmail.com>
2026-01-16 03:30:03 +0530

    added chips

--- src/app/tabs/tabs.ts
+++ src/app/tabs/tabs.ts
@@ -4,10 +4,11 @@ import {MatTabsModule} from "@angular/material/tabs";
 import {Table} from "../table/table";
 import {Checkboxes} from "../checkboxes/checkboxes";
 import {Buttons} from "../buttons/buttons";
+import {Chips} from "../chips/chips";
 
 @Component({
     selector: 'app-tabs',
     templateUrl: './tabs.html',
-    imports: [Loader, MatTabsModule, Table, Checkboxes, Buttons]
+    imports: [Loader, MatTabsModule, Table, Checkboxes, Buttons, Chips]
 })
 export class Tabs { }
