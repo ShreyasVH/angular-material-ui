dda0c7a3fcee14fe771fdbbd64a9445354310ccd
ShreyasVH <shreyas.deve.1@gmail.com>
2026-01-26 10:13:36 +0530

    drawer support

--- src/app/app-bar/app-bar.ts
+++ src/app/app-bar/app-bar.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 import {MatIconModule} from "@angular/material/icon";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { Tabs } from '../tabs/tabs';
 
 @Component({
   selector: 'app-bar',
   templateUrl: './app-bar.html',
+  styleUrls: ['./app-bar.css'],
   imports: [
       MatIconModule,
       MatToolbarModule,
-      MatButtonModule
+      MatButtonModule,
+      MatSidenavModule,
+      MatListModule,
+      Tabs
   ]
 })
 export class AppBar { }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import {AppBar} from "./app-bar/app-bar";
-import {Tabs} from "./tabs/tabs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrls: ['./app.css'],
   imports: [
-      AppBar,
-      Tabs
+      AppBar
   ]
 })
 export class App { }
